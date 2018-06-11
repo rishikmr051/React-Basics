2a766ad8a57aed14465174f6ff2e2bca9cbcef5f
rishikmr051 <>
2018-06-11 11:14:51 +0530

    na

---
+++ src/App.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import './App.css';
+
+
+interface IState {
+  title: string;
+  users: IUser[];
+}
+
+interface IUser {
+  name: string;
+}
+
+class App extends React.Component<{}, IState> {
+
+public state: Readonly<IState> = {
+  title: 'initial title',
+  users: [
+    {
+      name: 'Rajnikanth'
+    },
+    {
+      name: 'Salman'
+    },
+    {
+      name: 'Akshay'
+    },
+    {
+      name: 'Amitabh'
+    }
+  ]
+}
+
+  // public name = 'mycomponent';
+    
+  public onBtnClick = () => {
+    // alert(this.name);
+    // alert('Hello React');
+    this.setState({
+      title: 'new title'
+    });
+  }
+
+  public render() {
+
+const liItems = this.state.users.map(user => (<li>{user.name}</li>));
+
+    return (
+      <div className="App">
+        <ul>
+          {liItems}
+        </ul>
+      {this.state.title}
+        
+      <br /><button onClick={this.onBtnClick}>Click Me</button>
+      </div>
+    );
+  }
+}
+
+export default App;
