e56290f5d1a4303f90cfb9aeb3279002fa804b7f
rishikmr051 <40091516+rishikmr051@users.noreply.github.com>
2018-06-19 12:13:45 +0530

    Create justAdded.js

---
+++ justAdded.js
@@ -0,0 +1,15 @@
+npm install flux --save
+npm install @types/flux --save-dev
+
+npm install bootstrap --save
+
+npm install --save react-router
+npm install --save react-router-dom
+
+
+https://definitelytyped.org/
+jintoppy
+vinod.co
+kayartaya-vinod
+mockapi.io
+getbootstrap.com
