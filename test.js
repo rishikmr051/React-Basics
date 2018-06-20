60ea80e5c4cffa785cfe8de3e717fa600f548f61
rishikmr051 <40091516+rishikmr051@users.noreply.github.com>
2018-06-20 11:41:03 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,16 @@
+// ------------------------------
 Unit testing: 
   1.  Mocha + Chai + sinon
   2.  Jasmime
+
+  
+// ------------------------------
+Test runners
+  1.  Karma
+  
+// ------------------------------
+Jest: Testing framework on top of Jasmine
+
+
+// ------------------------------
+Enzyme: Library to render react component, find element and test
